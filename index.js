59066ab67071c5b268dfb6049f1cd4a5a2728737
ferizqoo <63696900+ferizqoo@users.noreply.github.com>
2020-11-05 16:09:10 +0700

    Update index.js

--- index.js
+++ index.js
@@ -47,61 +47,6 @@ client.on("guildMemberUpdate", async (oldState, newState) => {
 
 client.on("guildMemberAdd", member => {
 
-      if(!member.guild) return;
-    
-      const canvas = Canvas.createCanvas(1772, 633);
-     
-      const ctx = canvas.getContext('2d');
-  
-      const background = await Canvas.loadImage('https://cdn.discordapp.com/attachments/762823411184697395/771980958579949588/EHBannerR_3.png');
-      ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-      ctx.strokeStyle = '#f2f2f2';
-      ctx.strokeRect(0, 0, canvas.width, canvas.height);
-       
-      var textString3 = `${member.user.username}`;
-      
-      if (textString3.length >= 14) {
-        ctx.font = 'bold 100px Genta';
-        ctx.fillStyle = '#f2f2f2';
-        ctx.fillText(textString3, 720, canvas.height / 2 + 20);
-      }
-      
-      else {
-        ctx.font = 'bold 150px Genta';
-        ctx.fillStyle = '#f2f2f2';
-        ctx.fillText(textString3, 720, canvas.height / 2 + 20);
-      }
-     
-      var textString2 = `#${member.user.discriminator}`;
-      ctx.font = 'bold 40px Genta';
-      ctx.fillStyle = '#f2f2f2';
-      ctx.fillText(textString2, 730, canvas.height / 2 + 58);
-      
-      var textString4 = `Member #${member.guild.memberCount}`;
-      ctx.font = 'bold 60px Genta';
-      ctx.fillStyle = '#f2f2f2';
-      ctx.fillText(textString4, 750, canvas.height / 2 + 125);
-      
-      var textString4 = `${member.guild.name}`;
-      ctx.font = 'bold 60px Genta';
-      ctx.fillStyle = '#f2f2f2';
-      ctx.fillText(textString4, 700, canvas.height / 2 - 150);
-      
-      ctx.beginPath();
-      ctx.arc(315, canvas.height / 2, 250, 0, Math.PI * 2, true);//position of img
-      ctx.closePath();
-      ctx.clip();
-      
-      const avatar = await Canvas.loadImage(member.user.displayAvatarURL({ format: 'jpg' }));
-      ctx.drawImage(avatar, 65, canvas.height / 2 - 250, 500, 500);
-      
-      const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'welcome-image.png');
-      
-     
-      const channel = member.guild.channels.cache.find(ch => ch.id === "587018766173601794");
-      channel.send(attachment);
-
-  
 
 const meki = new Discord.MessageEmbed()
 .setTitle("Welcome!!")
